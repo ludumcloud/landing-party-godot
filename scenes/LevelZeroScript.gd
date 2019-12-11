1b89ba88b3b05d7b126bbac4a2539c8619c2d031
Mabry Cervin <mabry.cervin@gmail.com>
2019-12-11 17:35:02 -0500

    Added level zero script

---
+++ scenes/LevelZeroScript.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var tileMap = self.get_node("./TileMap")
+	print(tileMap.get_used_cells())
+	print(tileMap.map_to_world(Vector2(3, -1)))
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _draw():
+	var tileMap = self.get_node("./TileMap")
+	draw_circle(tileMap.map_to_world(Vector2(3, 1)), 10, Color.red)
