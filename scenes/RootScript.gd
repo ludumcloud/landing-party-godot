2ee5d6d4e51d39a245e823b43c1d616d8f422d22
Mark Abernathy <mark.abernathy@inin.com>
2020-02-07 17:21:14 -0500

    Adding scene transition to title level

--- scenes/RootScript.gd
+++ scenes/RootScript.gd
@@ -1,14 +1,5 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var levelZero = load("res://scenes/LevelZero.tscn")
-	self.add_child(levelZero.instance())
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _unhandled_input(event):
+	if event is InputEventKey:
+		get_tree().change_scene("res://scenes/LevelZero.tscn")
