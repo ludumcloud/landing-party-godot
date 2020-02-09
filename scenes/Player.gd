4d9879afe91f513b319d26bfd66da2f57a6a0f60
Mabry Cervin <mabry.cervin@gmail.com>
2020-02-09 00:04:11 -0500

    Momentary action tile now responds to player enter/exit

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -6,21 +6,16 @@ extends Node2D
 var next_pos = null
 var ENTITY_SPEED = 400 
 onready var sprite: AnimatedSprite = get_node('AnimatedSprite')
-
-func center_world_coords(position):
-	var map = get_parent().map
-	var converted = map.map_to_world(map.world_to_map(position)) 
-	var centered = converted + map.cell_size * 0.5
-	return centered
+onready var parent = get_parent();
 	
 func step_character(entity, destination_position, delta):
-	var points = get_parent().get_travel_path(entity, destination_position)
+	var points = parent.get_travel_path(entity, destination_position)
 	print(points)
 	var distance = ENTITY_SPEED * delta
 	if points.size() > 1:
 		var vector = points[1] - points[0]
 		if (vector.length() < distance) and (points.size() == 2):
-			entity.position = center_world_coords(points[1])
+			entity.position = parent.center_world_coords(points[1])
 			return true
 		var normalized = vector.normalized()
 		var magnitude = (normalized * 5)
@@ -28,7 +23,7 @@ func step_character(entity, destination_position, delta):
 		entity.position = new_position
 		return false
 	elif points.size() == 1:
-		entity.position = center_world_coords(points[0])
+		entity.position = parent.center_world_coords(points[0])
 		return true
 	else:
 		# Do nothing and mark completes
@@ -36,20 +31,22 @@ func step_character(entity, destination_position, delta):
 
 func move_to_pos(dest_tile_coords):
 	next_pos = dest_tile_coords
+	parent.redshirt_exited(world_coords_to_tile(self.position))
 
 func tile_coords_to_world(tile):
-	return get_parent().map.map_to_world(tile)
-
-# Called when the node enters the scene tree for the first time.
+	return parent.map.map_to_world(tile)
+	
+func world_coords_to_tile(world_coords):
+	return parent.map.world_to_map(world_coords)
 
 func _ready():
 	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _process(delta):
 	if next_pos:
 		sprite.play("walk")
 		var complete = step_character(self, tile_coords_to_world(next_pos), delta)
 		if complete:
 			sprite.play("idle")
+			parent.redshirt_entered(next_pos)
 			next_pos = null
